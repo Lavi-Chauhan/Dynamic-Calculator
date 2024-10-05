bea89618a1243bc979a11cec5e3ce55ccad8e71f
Lavi Chauhan <chauhanlavi2000@gmail.com>
2024-10-05 13:17:45 +0530

    Create CalculatorModel.cs

---
+++ Models/CalculatorModel.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DynamicCalculator.Models
+{
+    public class CalculatorModel
+    {
+        public double Number1 { get; set; }
+        public double Number2 { get; set; }
+        public string Operation { get; set; }
+        public double Result { get; set; }
+    }
+}
